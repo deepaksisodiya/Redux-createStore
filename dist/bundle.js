06b9b616f88a073aa6b16480e4a95ac6921d705b
Deepak Sisodiya <deepaksisodiya@Deepaks-MacBook-Pro.local>
2015-12-13 19:31:20 +0530

    implementation of createStore method

--- dist/bundle.js
+++ dist/bundle.js
@@ -65,6 +65,31 @@
 	var createStore = __webpack_require__(1).createStore;
 	// import { createStore } from 'redux';
 
+	/*// this is the implementation of createStore method
+	const createStore = (reducer) => {
+	  "use strict";
+	  let state;
+	  let listeners = [];
+
+	  const getState = () => state;
+
+	  const dispatch = (action) => {
+	    state = reducer(state, action);
+	    listeners.forEach(listener => listener());
+	  };
+
+	  const subscribe = (listener) => {
+	    listeners.push(listener);
+	    return () => {
+	      listeners = listeners.filter(l => l !== listener);
+	    }
+	  };
+
+	  dispatch({});
+
+	  return { getState, dispatch, subscribe };
+	};*/
+
 	var store = createStore(counter);
 
 	document.addEventListener('click', () => {
--- src/main.js
+++ src/main.js
@@ -19,6 +19,31 @@ function counter(state, action) {
 var createStore = require('redux').createStore;
 // import { createStore } from 'redux';
 
+/*// this is the implementation of createStore method
+const createStore = (reducer) => {
+  "use strict";
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach(listener => listener());
+  };
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter(l => l !== listener);
+    }
+  };
+
+  dispatch({});
+
+  return { getState, dispatch, subscribe };
+};*/
+
 var store = createStore(counter);
 
 document.addEventListener('click', () => {
